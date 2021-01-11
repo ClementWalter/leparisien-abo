1521939886fa5a4296dde96cdf4e0e2977cd804b
Clément Walter <clement0walter@gmail.com>
2021-01-11 11:13:39 +0100

    Update cleanPage

--- content.js
+++ content.js
@@ -4,14 +4,13 @@ const cleanPage = (pageTitle) => {
   Array.from(document.styleSheets).forEach(x => x.insertRule('.blurText { filter: blur(0px) !important; }'));
   // Remove any paywall modals
   document.querySelectorAll('.piano-paywall').forEach(x => x.remove());
-};
+}
 
 chrome.runtime.onMessage.addListener(function (message) {
-  // 3 delayed attempts to compensate possible race conditions (css stylesheets could load slowly)
-  if (message.title) {
+  if ( message.title ) {
     const timeouts = [1000, 2000, 3000, 4000, 5000, 10000];
     let timeout;
-    for (timeout in timeouts) {
+    for ( timeout in timeouts ) {
       window.setTimeout(() => cleanPage(message.title), timeouts[timeout])
     }
   }
