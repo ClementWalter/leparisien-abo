346d7a80663afee9b5c58ce498f2faed92ebfe35
Clément Walter <clement0walter@gmail.com>
2021-01-11 11:22:19 +0100

    Fix https

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
 // Regex-pattern to check URLs against.
 // It matches URLs like: http[s]://[...]stackoverflow.com[...]
-const leParisienUrlRegex = /^http?:\/\/(?:[^./?#]+\.)?leparisien\.fr/;
+const leParisienUrlRegex = /^https?:\/\/(?:[^./?#]+\.)?leparisien\.fr/;
 let leParisienTabIds = [];
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	const title = tab.title;
+	console.log("title", title)
+	console.log("url", tab.url)
 	if(leParisienUrlRegex.test(tab.url)){
+	  console.log("leParisienTabIds", leParisienTabIds)
 	  leParisienTabIds.push(tabId);
 	  leParisienTabIds = [...new Set(leParisienTabIds)];
+	  console.log("leParisienTabIds", leParisienTabIds)
 	  chrome.storage.sync.set({leParisienTabIds}, function() {
       console.log('LeParisien tab id set to ' + leParisienTabIds);
       chrome.tabs.sendMessage(tab.id, {title});
